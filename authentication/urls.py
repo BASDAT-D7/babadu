001e694de12e38a4a6b76519e6ae69cfcb49b728
eugeniusms <eugeniusms@gmail.com>
2023-05-06 13:37:35 +0700

    🛠 Register Atlet Page Done

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import authentication, register, login, logout
+from authentication.views import *
 
 app_name = 'authentication'
 
 urlpatterns = [
   path('', authentication, name='authentication'),
   path('register/', register, name='register'),
+  path('register/atlet/', register_atlet, name='register_atlet'),
+  path('register/pelatih/', register_pelatih, name='register_pelatih'),
+  path('register/umpire/', register_umpire, name='register_umpire'),
   path('login/', login, name='login'),
   path('logout/', logout, name='logout'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def authentication(request):
-    return render(request, 'authentication.html')
+  return render(request, 'authentication.html')
 
 def register(request):
   return render(request, 'register.html')
 
+def register_atlet(request):
+  return render(request, 'register_form/register_atlet.html')
+
+def register_pelatih(request):
+  return render(request, 'register_form/register_pelatih.html')
+
+def register_umpire(request):
+  return render(request, 'register_form/register_umpire.html')
+
 def login(request):
-    return render(request, 'login.html')
+  return render(request, 'login.html')
 
 def logout(request):
-    return render(request, 'logout.html')
+  return render(request, 'logout.html')
