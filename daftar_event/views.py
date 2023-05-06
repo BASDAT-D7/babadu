11ce07e7798ef64a8ffd046bd367b0444babb98c
eugeniusms <eugeniusms@gmail.com>
2023-05-06 17:40:53 +0700

    🛠 Daftar Event By Stadium (Step 2) UI Page Done

--- daftar_event/views.py
+++ daftar_event/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def daftar_stadium(request):
-    dummy_daftar_event = {
-        'daftar_event': [
+    dummy_daftar_stadium = {
+        'daftar_stadium': [
             {
                 'id': 1,
                 'nama_stadium': 'Gelora Bung Karno',
@@ -30,34 +30,55 @@ def daftar_stadium(request):
             }
         ]
     }
-    return render(request, 'daftar_stadium.html', dummy_daftar_event)
+    return render(request, 'daftar_stadium.html', dummy_daftar_stadium)
 
 def daftar_event_by_stadium(request, id):
     dummy_daftar_event = {
         'daftar_event': [
             {
                 'id': 1,
-                'nama_stadium': 'Gelora Bung Karno',
-                'negara': 'Indonesia',
-                'kapasitas': '8'
+                'nama_event': 'Badminton Open',
+                'total_hadiah': 'Rp 5.000.000',
+                'tanggal_mulai': '12-04-2023',
+                'kategori': 'S200',
+                'kapasitas_terisi': 3,
+                'kapasitas_total': 8
             },
             {
                 'id': 2,
-                'nama_stadium': 'Senayan',
-                'negara': 'Indonesia',
-                'kapasitas': '16'
-            }, 
+                'nama_event': 'Bali Open',
+                'total_hadiah': 'Rp 4.000.000',
+                'tanggal_mulai': '27-04-2023',
+                'kategori': 'S100',
+                'kapasitas_terisi': 8,
+                'kapasitas_total': 8
+            },
             {
                 'id': 3,
-                'nama_stadium': 'Old Trafford',
-                'negara': 'Inggris',
-                'kapasitas': '74'
+                'nama_event': 'Jakarta Invitational',
+                'total_hadiah': 'Rp 7.500.000',
+                'tanggal_mulai': '15-05-2023',
+                'kategori': 'S300',
+                'kapasitas_terisi': 4,
+                'kapasitas_total': 10
             },
             {
                 'id': 4,
-                'nama_stadium': 'Maracanã',
-                'negara': 'Brasil',
-                'kapasitas': '78'
+                'nama_event': 'Surabaya Championship',
+                'total_hadiah': 'Rp 3.500.000',
+                'tanggal_mulai': '02-06-2023',
+                'kategori': 'S150',
+                'kapasitas_terisi': 6,
+                'kapasitas_total': 10
+            },
+            {
+                'id': 5,
+                'nama_event': 'Yogyakarta Masters',
+                'total_hadiah': 'Rp 6.000.000',
+                'tanggal_mulai': '18-06-2023',
+                'kategori': 'S250',
+                'kapasitas_terisi': 2,
+                'kapasitas_total': 6
             }
         ]
     }
