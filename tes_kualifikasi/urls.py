649cd3dff39c415fea4ede020183a632a9c5bc49
eugeniusms <eugeniusms@gmail.com>
2023-05-06 15:46:38 +0700

    🛠 Form Data Kualifikasi UI Page Done

--- tes_kualifikasi/urls.py
+++ tes_kualifikasi/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from tes_kualifikasi.views import *
+
 app_name = 'tes_kualifikasi'
 
-urlpatterns = []
+urlpatterns = [
+    path('', form_data_kualifikasi, name='form_data_kualifikasi'),
+    path('pertanyaan/', pertanyaan_kualifikasi, name='pertanyaan_kualifikasi')
+]
--- tes_kualifikasi/views.py
+++ tes_kualifikasi/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
 
 # Create your views here.
+def form_data_kualifikasi(request):
+    dummy_form_settings = {
+        "tempat_pelaksanaan": [
+            "Gor Bulutangkis Jakarta",
+            "Gor Bulutangkis Surabaya",
+            "Gor Bulutangkis Bandung"
+        ],
+        "tanggal_pelaksanaan": [
+            "06/05/2023",
+            "07/05/2023",
+            "08/05/2023",
+            "09/05/2023",
+            "10/05/2023"
+        ]
+
+    }
+    return render(request, 'form_data_kualifikasi.html', dummy_form_settings)
+
+def pertanyaan_kualifikasi(request):
+    dummy_pertanyaan = {
+        "pertanyaan": [
+            {
+                "pertanyaan": "Apakah anda pernah mengikuti kejuaraan bulutangkis sebelumnya?",
+                "jawaban": [
+                    "Ya",
+                    "Tidak"
+                ]
+            },
+            {
+                "pertanyaan": "Bagaimana hasil kejuaraan bulutangkis sebelumnya?",
+                "jawaban": [
+                    "Juara",
+                    "Runner Up",
+                    "Semi Final",
+                    "Perempat Final",
+                    "Tidak Sama Sekali"
+                ]
+            },
+            {
+                "pertanyaan": "Mengapa anda ingin mengikuti kejuaraan bulutangkis ini?",
+                "jawaban": [
+                    "Untuk mengasah kemampuan",
+                    "Untuk mengisi waktu luang",
+                    "Untuk mengikuti jejak orang tua",
+                    "Alasan lainnya"
+                ]
+            },
+            {
+                "pertanyaan": "Berapa lama anda bermain bulutangkis?",
+                "jawaban": [
+                    "Kurang dari 1 tahun",
+                    "1 - 2 tahun",
+                    "2 - 3 tahun",
+                    "Lebih dari 3 tahun"
+                ]
+            }
+        ]
+    }
+    return render(request, 'pertanyaan_kualifikasi.html', dummy_pertanyaan)
