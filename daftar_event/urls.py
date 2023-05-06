00a57a51292b36b1c74d9ac1319d1364bfa759fa
eugeniusms <eugeniusms@gmail.com>
2023-05-06 17:16:52 +0700

    🛠 Daftar Event (Step 1) UI Page Done

--- daftar_event/urls.py
+++ daftar_event/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from daftar_event.views import *
+
 app_name = 'daftar_event'
 
-urlpatterns = []
+urlpatterns = [
+    path('', daftar_event, name='daftar_event')
+]
--- daftar_event/views.py
+++ daftar_event/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+def daftar_event(request):
+    dummy_daftar_event = {
+        'daftar_event': [
+            {
+                'id': 1,
+                'nama_stadium': 'Gelora Bung Karno',
+                'negara': 'Indonesia',
+                'kapasitas': '8'
+            },
+            {
+                'id': 2,
+                'nama_stadium': 'Senayan',
+                'negara': 'Indonesia',
+                'kapasitas': '16'
+            }, 
+            {
+                'id': 3,
+                'nama_stadium': 'Old Trafford',
+                'negara': 'Inggris',
+                'kapasitas': '74'
+            },
+            {
+                'id': 4,
+                'nama_stadium': 'Maracanã',
+                'negara': 'Brasil',
+                'kapasitas': '78'
+            }
+        ]
+    }
+    return render(request, 'daftar_event.html', dummy_daftar_event)
