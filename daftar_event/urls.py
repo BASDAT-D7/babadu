39437158e8241033375ffbcd8e0481d9a6888278
eugeniusms <eugeniusms@gmail.com>
2023-05-06 17:26:07 +0700

    🛠 Create Project Structure For Daftar Event

--- daftar_event/urls.py
+++ daftar_event/urls.py
@@ -4,5 +4,7 @@ from daftar_event.views import *
 app_name = 'daftar_event'
 
 urlpatterns = [
-    path('', daftar_event, name='daftar_event')
+    path('', daftar_stadium, name='daftar_event'),
+    path('stadium/<int:id>/', daftar_event_by_stadium, name='daftar_event_by_stadium'),
+    path('stadium/<int:id>/kategori/', daftar_kategori, name="daftar_kategori")
 ]
--- daftar_event/views.py
+++ daftar_event/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def daftar_event(request):
+def daftar_stadium(request):
     dummy_daftar_event = {
         'daftar_event': [
             {
@@ -30,4 +30,66 @@ def daftar_event(request):
             }
         ]
     }
-    return render(request, 'daftar_event.html', dummy_daftar_event)
+    return render(request, 'daftar_stadium.html', dummy_daftar_event)
+
+def daftar_event_by_stadium(request, id):
+    dummy_daftar_event = {
+        'daftar_event': [
+            {
+                'id': 1,
+                'nama_stadium': 'Gelora Bung Karno',
+                'negara': 'Indonesia',
+                'kapasitas': '8'
+            },
+            {
+                'id': 2,
+                'nama_stadium': 'Senayan',
+                'negara': 'Indonesia',
+                'kapasitas': '16'
+            }, 
+            {
+                'id': 3,
+                'nama_stadium': 'Old Trafford',
+                'negara': 'Inggris',
+                'kapasitas': '74'
+            },
+            {
+                'id': 4,
+                'nama_stadium': 'Maracanã',
+                'negara': 'Brasil',
+                'kapasitas': '78'
+            }
+        ]
+    }
+    return render(request, 'daftar_event_by_stadium.html', dummy_daftar_event)
+
+def daftar_kategori(request, id):
+    dummy_daftar_event = {
+        'daftar_event': [
+            {
+                'id': 1,
+                'nama_stadium': 'Gelora Bung Karno',
+                'negara': 'Indonesia',
+                'kapasitas': '8'
+            },
+            {
+                'id': 2,
+                'nama_stadium': 'Senayan',
+                'negara': 'Indonesia',
+                'kapasitas': '16'
+            }, 
+            {
+                'id': 3,
+                'nama_stadium': 'Old Trafford',
+                'negara': 'Inggris',
+                'kapasitas': '74'
+            },
+            {
+                'id': 4,
+                'nama_stadium': 'Maracanã',
+                'negara': 'Brasil',
+                'kapasitas': '78'
+            }
+        ]
+    }
+    return render(request, 'daftar_kategori.html', dummy_daftar_event)
