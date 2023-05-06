19a475f81e0fbd64fe0228f0f03e7b044d9efd51
eugeniusms <eugeniusms@gmail.com>
2023-05-06 16:14:51 +0700

    🛠 Daftar Atlet UI Page Done

--- daftar_atlet/urls.py
+++ daftar_atlet/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from daftar_atlet.views import *
+
 app_name = 'daftar_atlet'
 
-urlpatterns = []
+urlpatterns = [
+    path('', daftar_atlet, name='daftar_atlet')
+]
--- daftar_atlet/views.py
+++ daftar_atlet/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def daftar_atlet(request):
+    dummy_daftar_atlet = {
+        "daftar_atlet": [
+            {
+                "nama_lengkap": "Eugenius Mario Situmorang",
+            },
+            {
+                "nama_lengkap": "Peokra Mokra",
+            },
+            {
+                "nama_lengkap": "Aegon Targaryen",
+            },
+        ]  
+    }
+    return render(request, 'daftar_atlet.html', dummy_daftar_atlet)
