431868243e67db7701536aa25448c37a92ec5f22
eugeniusms <eugeniusms@gmail.com>
2023-05-06 16:51:21 +0700

    🛠 List Atlet UI Page Done

--- list_atlet/urls.py
+++ list_atlet/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from list_atlet.views import *
+
 app_name = 'list_atlet'
 
-urlpatterns = []
+urlpatterns = [
+    path('', list_atlet, name='list_atlet')
+]
--- list_atlet/views.py
+++ list_atlet/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_atlet(request):
+    dummy_list_atlet = {
+        "list_atlet": [
+            {
+                "nama_lengkap": "Aegon Targaryen",
+                "email": "aegon@got.com",
+                "world_rank": "#1",
+            },
+            {
+                "nama_lengkap": "Daenerys Targaryen",
+                "email": "daenerys@got.com",
+                "world_rank": "#16",
+            },
+            {
+                "nama_lengkap": "Eugenius Mario Situmorang",
+                "email": "eugeniusms@gmail.com",
+                "world_rank": "#1182",
+            },
+            {
+                "nama_lengkap": "Tyrion Lannister",
+                "email": "tyrion@got.com",
+                "world_rank": "#2126",
+            },
+            {
+                "nama_lengkap": "Peokra Mokra",
+                "email": "peokramokra@gmail.com",
+                "world_rank": "#5432",
+            },
+        ]
+    }
+    return render(request, 'list_atlet.html', dummy_list_atlet)
