2d58067c3f490abbb376b00001acfa417aa560e2
eugeniusms <eugeniusms@gmail.com>
2023-05-17 15:43:14 +0700

    feat: navbar dropdown atlet

--- static/setNavbar.js
+++ static/setNavbar.js
@@ -7,17 +7,35 @@ function setNavbar(is_authenticated) {
                 <div class="flex gap-5 font-semibold text-bluebright text-xl">
                     Babadu.D7
                 </div>  
-                <div class="flex gap-5">
+                <div class="flex gap-5 items-center">
                     <button class="hover:text-blue"><a href="/">Dashboard</a></button>
-                    <button class="hover:text-blue"><a href="/tes-kualifikasi">Tes Kualifikasi</a></button>
-                    <button class="hover:text-blue"><a href="/daftar-event">Daftar Event</a></button>
-                    <button class="hover:text-blue"><a href="/enrolled-event">Enrolled Event</a></button>
-                    <button class="hover:text-blue"><a href="/daftar-sponsor">Daftar Sponsor</a></button>
+                    <div class="dropdown">
+                      <button class="hover:text-blue">Ujian Kualifikasi</button>
+                      <div class="dropdown-content">
+                        <a href="/tes-kualifikasi">List Ujian Kualifikasi</a>
+                        <a href="/riwayat-ujian-kualifikasi">Riwayat Ujian Kualifikasi</a>
+                      </div>
+                    </div>
+                    <div class="dropdown">
+                      <button class="hover:text-blue">Event</button>
+                      <div class="dropdown-content">
+                        <a href="/daftar-event">Daftar Event</a>
+                        <a href="/enrolled-event">Enrolled Event</a>
+                      </div>
+                    </div>
+                    <div class="dropdown">
+                      <button class="hover:text-blue">Sponsor</button>
+                      <div class="dropdown-content">
+                        <a href="/daftar-sponsor">Daftar Sponsor</a>
+                        <a href="/list-sponsor">List Sponsor</a>
+                      </div>
+                    </div>
                     <a href="/authentication/logout/">
                         <button class="border-2 border-redbright text-redbright rounded-xl py-2 px-4 hover:bg-redbright hover:text-black duration-200">Logout</button>
                     </a>
                 </div>
-            </div>`;
+            </div>
+            `;
     } else if (userRole == "PELATIH") {
       // NAVBAR PELATIH
       return `<div class="flex justify-between bg-black/50 backdrop-blur-sm text-white font-poppins items-center py-4 px-8 cursor-pointer top-0 z-50 w-full fixed border-b-[1px] border-white/20">
