3391a2472fc493504a14d41e76854a1fd2b8db08
eugeniusms <eugeniusms@gmail.com>
2023-05-17 14:05:32 +0700

    feat: dashboard done

--- dashboard/views.py
+++ dashboard/views.py
@@ -20,7 +20,7 @@ def dashboard(request):
         play = "Right" if parse(query_result(f"SELECT play_right FROM atlet WHERE id='{user_id}';")) else "Left"
         tinggi_badan = parse(query_result(f"SELECT height FROM atlet WHERE id='{user_id}';"))
         jenis_kelamin = "Laki" if parse(query_result(f"SELECT jenis_kelamin FROM atlet WHERE id='{user_id}';")) else "Perempuan"
-        world_rank = "-" if parse(query_result(f"SELECT world_rank FROM atlet WHERE id='{user_id}';")) == None else "#"+parse(query_result(f"SELECT world_rank FROM atlet WHERE id='{user_id}';"))
+        world_rank = "-" if parse(query_result(f"SELECT world_rank FROM atlet WHERE id='{user_id}';")) == None else "#"+str(parse(query_result(f"SELECT world_rank FROM atlet WHERE id='{user_id}';")))
         pelatih = parse(query_result(f"""
                                         SELECT nama
                                         FROM member
@@ -29,7 +29,8 @@ def dashboard(request):
                                         """
                                     ))
         print(query_result(f"SELECT * FROM atlet_non_kualifikasi WHERE id_atlet='{user_id}';"))
-        # status = "Qualified" if len(query_result(f"SELECT * FROM atlet_non_kualifikasi WHERE id_atlet='{user_id}';")) != 0 else "Not Qualified"
+        status = "Qualified" if len(query_result(f"SELECT * FROM atlet_non_kualifikasi WHERE id_atlet='{user_id}';")) != 0 else "Not Qualified"
+        total_poin = '0' if parse(query_result(f"SELECT SUM(total_point) FROM point_history WHERE id_atlet='{user_id}';")) == None else parse(query_result(f"SELECT SUM(total_point) FROM point_history WHERE id_atlet='{user_id}';"))
         # SET CONTEXT
         dummy_atlet = {
             "nama_lengkap": nama_lengkap,
@@ -42,7 +43,7 @@ def dashboard(request):
             "pelatih": pelatih,
             "status": status,
             "world_rank": world_rank,
-            "total_poin": "82150",
+            "total_poin": total_poin,
         }
         return render(request, 'dashboard_atlet.html', dummy_atlet)
     
