4262650e4d90a5986728892e7637b214fe07a6ac
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2023-05-23 21:48:42 +0700

    feat : finish SELECT query for daftar_event

--- daftar_event/urls.py
+++ daftar_event/urls.py
@@ -6,5 +6,5 @@ app_name = 'daftar_event'
 urlpatterns = [
     path('', daftar_stadium, name='daftar_event'),
     path('stadium/<str:nama_stadium>/', daftar_event_by_stadium, name='daftar_event_by_stadium'),
-    path('stadium/<str:nama_stadium>/event/<int:event_id>/', daftar_kategori, name="daftar_kategori")
+    path('stadium/<str:nama_stadium>/event/<str:nama_event>/', daftar_kategori, name="daftar_kategori")
 ]
--- daftar_event/views.py
+++ daftar_event/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from babadu_function.general import *
 from babadu_function.authentication import *
 from datetime import date
-import locale
 
 # Create your views here.
 @role_required(['ATLET'])
@@ -60,24 +59,32 @@ def daftar_event_by_stadium(request, nama_stadium):
             'tanggal_mulai': row[2],
             'kategori': row[3],
             'kapasitas_terisi': row[5],
-            'kapasitas_total': row[4]
+            'kapasitas_total': row[4],
+            'nama_stadium' : nama_stadium
         })
 
     return render(request, 'daftar_event_by_stadium.html', list_event_by_stadium)
 
 @role_required(['ATLET'])
 def daftar_kategori(request, nama_stadium, nama_event):
-    result_stadium = query_result(f'''
-        SELECT nama, negara
-        FROM STADIUM
-        WHERE nama = '{nama_stadium}';
-        ''')
 
-    result_event = query_result(f'''
-        SELECT E.nama_event, E.total_hadiah, E.tgl_mulai, E.tgl_selesai, E.kategori_superseries
+    user_id = request.COOKIES.get('user_id')
+
+    result_current_user = query_result(f'''
+        SELECT jenis_kelamin
+        FROM ATLET
+        WHERE id = '{user_id}';
+        ''')
+    
+    jenis_kelamin = result_current_user[0][0]
+
+    result_stadium_event = query_result(f'''
+        SELECT DISTINCT E.nama_event, E.total_hadiah, E.tgl_mulai, E.tgl_selesai, E.kategori_superseries, COUNT(*) as jumlah, S.kapasitas, S.nama, S.negara
         FROM EVENT as E
+        JOIN PARTAI_PESERTA_KOMPETISI as PPK ON E.nama_event = PPK.nama_event
+        JOIN STADIUM as S ON E.nama_stadium = S.nama
         AND E.nama_stadium = '{nama_stadium}'
-        AND E.nama_event = '{nama_event}';
+        GROUP BY E.nama_event, E.total_hadiah, E.tgl_mulai, E.kategori_superseries, S.kapasitas, E.tgl_selesai, E.kategori_superseries, S.nama, S.negara;
         ''')
 
     result_kategori = query_result(f'''
@@ -87,39 +94,58 @@ def daftar_kategori(request, nama_stadium, nama_event):
         GROUP BY jenis_partai, nama_event;
         ''')
 
-    result_atlet_tanpa_pasangan = query_result(f'''
-        SELECT
+    result_pemain_tunggal = query_result(f'''
+        SELECT DISTINCT COALESCE(STRING_AGG(M.nama, ', '), '-') as nama_pasangan, PPK.jenis_partai, PPK.nama_event 
+        FROM PARTAI_PESERTA_KOMPETISI as PPK
+        JOIN PESERTA_KOMPETISI as PK ON PK.nomor_peserta = PPK.nomor_peserta
+        JOIN ATLET_GANDA as AG ON AG.id_atlet_ganda = PK.id_atlet_ganda
+        JOIN ATLET as A ON AG.id_atlet_kualifikasi= A.id
+        JOIN MEMBER as M ON A.id = M.id
+        WHERE PPK.nama_event = '{nama_event}'
+        AND A.jenis_kelamin = '{jenis_kelamin}'
+        AND PK.id_atlet_ganda IN (
+            SELECT PK2.id_atlet_ganda
+            FROM PESERTA_KOMPETISI as PK2
+            GROUP BY PK2.id_atlet_ganda
+            HAVING COUNT(*) = 1
+        )
+        GROUP BY PPK.jenis_partai, PPK.nama_event;
         ''')
 
     list_kategori_by_event = {
-            'stadium' :[],
-            'event': [],
-            'daftar_kategori' : []
+            'stadium_event' :[],
+            'daftar_kategori' : [],
+            'daftar_pemain' : [],
+            'jenis_kelamin' : jenis_kelamin
         }
 
-    dummy_daftar_event = {
-        'stadium': "",
-        'event': "",
-        'daftar_kategori': [
-            {
-                "kategori": "Tunggal Putra",
-                "partner": "-"
-            },
-            {
-                "kategori": "Ganda Putra",
-                "partner": [
-                    "Kevin Sanjaya",
-                    "Marcus Gideon"
-                ]
-            }, 
-            {
-                "kategori": "Ganda Campuran",
-                "partner": [
-                    "Liliyana Natsir",
-                    "Debby Susanto"
-                ]
-            }
-        ]
 
-    }
-    return render(request, 'daftar_kategori.html', dummy_daftar_event)
+    for row in result_stadium_event:
+        list_kategori_by_event["stadium_event"].append({
+            'nama_event': row[0],
+            'total_hadiah': "Rp" + format(row[1], ","),
+            'tanggal_mulai': row[2],
+            'tanggal_selesai': row[3],
+            'kategori': row[4],
+            'kapasitas_terisi': row[5],
+            'kapasitas_total': row[6],
+            'nama_stadium' : row[7],
+            'negara' : row[8]
+        })
+    
+    
+    for row in result_kategori:
+        list_kategori_by_event["daftar_kategori"].append({
+            'kategori': row[0],
+            'jumlah_peserta' : row[1]
+        })
+
+    for row in result_pemain_tunggal:
+        pasangan = row[0].split(',')
+        list_kategori_by_event["daftar_pemain"].append({
+            'pasangan': pasangan,
+            'partai' : row[1]
+        })
+
+
+    return render(request, 'daftar_kategori.html', list_kategori_by_event)
