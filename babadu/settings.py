ed0959333ba91244ea33673b709cafd535e88c44
eugeniusms <eugeniusms@gmail.com>
2023-05-26 00:52:44 +0700

    done: database connection

--- babadu/settings.py
+++ babadu/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = "babadu.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'badminton',
+        'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': 'wJAHKbbicSnC0svrgCfT',
         'HOST': 'containers-us-west-125.railway.app',
